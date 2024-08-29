0959e6ee172f5c381209e679906520c2c03880a0
Mukesh kumar <108401017+mukeshkr123@users.noreply.github.com>
2024-08-29 22:02:55 +0530

    added category, image and product

---
+++ src/main/java/com/shopping_cart/model/Image.java
@@ -0,0 +1,30 @@
+package com.shopping_cart.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Blob;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+    private String fileType;
+    private Blob image;
+
+    @Lob
+    private String downloadUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
